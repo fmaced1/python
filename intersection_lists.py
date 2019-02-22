05bf9f1261e8bbbc12d82297265002215a838e47
Fabricio Correa <39250521+fmaced1@users.noreply.github.com>
2019-02-21 21:19:04 -0300

    Update intersection_lists.py

--- intersection_lists.py
+++ intersection_lists.py
@@ -1,15 +1,5 @@
 import random
 
-print("""
-Take two lists, say for example these two:
-
-a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-
-and write a program that returns a list that contains only
-the elements that are common between the lists (without duplicates).
-Make sure your program works on two lists of different sizes.""")
-
 def newLine(lines):
     print((lines -1) * "\n")
 
